9725ebbd2edbb140e7d1d78fee4e4f7df7c378e5
NegativeVibes <xavier_rate@outlook.com>
2021-06-04 08:30:03 +1000

    Highscore started... and title text change

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,19 +8,21 @@ public class GameManager : MonoBehaviour
 {
     public GameObject platformPrefab;
 
-    public float platformCount = Mathf.Infinity;
+    public int numberOfPlatforms;
+    public float levelWidth = 3f;
+    public float minY = .2f;
+    public float maxY = 1.5f;
     
     // Start is called before the first frame update
     void Start()
     {
         Vector3 spawnPosition = new Vector3();
-
-        for (int i = 0; i < platformCount; i++)
+        
+        for (int i = 0; i < numberOfPlatforms; i++)
         {
-            spawnPosition.y += Random.Range(5f, 2f);
-            spawnPosition.x = Random.Range(-5f, 5f);
+            spawnPosition.y = +Random.Range(minY, maxY);
+            spawnPosition.x = Random.Range(-levelWidth, levelWidth);
             Instantiate(platformPrefab, spawnPosition, quaternion.identity);
         }
-
     }
 }
