2391c7e462ba4b6722309abd0b68ded2054895f3
Xavier Arias <xavier_rate@outlook.com>
2021-06-09 16:27:53 +1000

    Animation functionality works now to add visual asthetics

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -11,10 +11,13 @@ public class Platform : MonoBehaviour
     {
         if (collision.relativeVelocity.y <= 0f)
         {
+            //collision.gameObject.GetComponent<Animator>().SetTrigger("Landing");
+            collision.gameObject.GetComponent<PlayerController>().AnimDelay();
             UIHandler.score += 1;
             Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();
             if (rb != null)
             {
+              //  gameObject.GetComponent<Animator>().SetTrigger("Jumping");
                 Vector2 velocity = rb.velocity;
                 velocity.y = jumpForce;
                 rb.velocity = velocity;
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,19 +6,47 @@ using UnityEngine.PlayerLoop;
 
 public class PlayerController : MonoBehaviour
 {
+    public Vector2 vel; // the velocity of player
     public float moveSpeed = 10;
     public Rigidbody2D rb;
-
     private float moveX;
-
+    private Animator anim;
+    
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
     {
         moveX = Input.GetAxis("Horizontal") * moveSpeed;
+    }
+
+    //Triggers on impact with platform
+    public void AnimDelay()
+    {
+        anim.SetTrigger("Landing");
+        Invoke("Bounce", 0.1f);
+    }
+
+    void Bounce()
+    {
+        anim.SetTrigger("Jumping");
+
+        Invoke("FallDelay", 0.75f);
+
+    }
+    
+    void FallDelay()
+    {
+        anim.SetTrigger("Falling");
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        //anim.SetTrigger("Landing");
     }
 
     // Start is called before the first frame update
